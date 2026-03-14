7c8922e1ba77b5a220b20ef385a5568d87fa2e26
DuongTuanKhang <khangnha0811@gmail.com>
2026-03-14 22:26:03 +0700

    add responsive

--- main.js
+++ main.js
@@ -1,233 +1,237 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-Modal.elements = [];
+Modix.elements = [];
 
-function Modal(options = {}) {
-  const {
-    templateId,
-    destroyOnClose = true,
-    footer = false,
-    closeMethods = ["close", "overlay", "escape"],
-    cssClass = [],
-    onOpen,
-    onClose,
-  } = options;
-  const template = $(`#${templateId}`);
+function Modix(options = {}) {
+  this.opt = Object.assign(
+    {
+      destroyOnClose: true,
+      footer: false,
+      cssClass: [],
+      closeMethods: ["button", "overlay", "escape"],
+    },
+    options,
+  );
+  this.template = $(`#${this.opt.templateId}`);
 
-  if (!template) {
-    console.error(`#${templateId} does not exist!`);
+  if (!this.template) {
+    console.error(`#${this.opt.templateId} does not exist!`);
     return;
   }
 
+  const { closeMethods } = this.opt;
+  this._allowButtonClose = closeMethods.includes("button");
   this._allowBackdropClose = closeMethods.includes("overlay");
-  this._allowButtonClose = closeMethods.includes("close");
   this._allowEscapeClose = closeMethods.includes("escape");
-
-  this._getScrollbarWidth = () => {
-    if (this._scrollbarWidth) return this._scrollbarWidth;
-    const div = document.createElement("div");
-    Object.assign(div.style, {
-      overflow: "scroll",
-      position: "absolute",
-      top: "-999px",
-    });
-
-    document.body.appendChild(div);
-
-    this._scrollbarWidth = div.offsetWidth - div.clientWidth;
-
-    document.body.removeChild(div);
-
-    console.log("Tính toán kích thước thanh cuộn: ", this._scrollbarWidth);
-    return this._scrollbarWidth;
-  };
-
-  this._build = () => {
-    const content = template.content.cloneNode(true);
-
-    // Create modal elements
-    this._backdrop = document.createElement("div");
-    this._backdrop.className = "modal-backdrop";
-
-    const container = document.createElement("div");
-    container.className = "modal-container";
-
-    cssClass.forEach((className) => {
-      if (typeof className === "string") {
-        container.classList.add(className);
-      }
-    });
-
-    if (this._allowButtonClose) {
-      const closeBtn = this._createButton("&times;", "modal-close", this.close);
-      container.append(closeBtn);
-    }
-
-    const modalContent = document.createElement("div");
-    modalContent.className = "modal-content";
-
-    // Append content and elements
-    modalContent.append(content);
-    container.append(modalContent);
-
-    if (footer) {
-      this._modalFooter = document.createElement("div");
-      this._modalFooter.className = "modal-footer";
-
-      this._renderFooterContent();
-      this._renderFooterButton();
-      modalContent.append(this._modalFooter);
-    }
-
-    this._backdrop.append(container);
-    document.body.append(this._backdrop);
-  };
-
-  this.setFooterContent = (html) => {
-    this._footerContent = html;
-    this._renderFooterContent();
-  };
 
   this._footerButtons = [];
 
-  this.addFooterButton = (title, cssClass, callback) => {
-    const button = this._createButton(title, cssClass, callback);
-    this._footerButtons.push(button);
-    this._renderFooterButton();
-  };
-
-  this._renderFooterButton = () => {
-    if (this._modalFooter) {
-      this._footerButtons.forEach((button) => {
-        this._modalFooter.append(button);
-      });
-    }
-  };
-
-  this._renderFooterContent = () => {
-    if (this._modalFooter && this._footerContent) {
-      this._modalFooter.innerHTML = this._footerContent;
-    }
-  };
-
-  this._createButton = (title, cssClass, callback) => {
-    const button = document.createElement("button");
-    button.className = cssClass;
-    button.innerHTML = title;
-    button.onclick = callback;
-
-    return button;
-  };
-
-  this.open = () => {
-    Modal.elements.push(this);
-
-    if (!this._backdrop) {
-      this._build();
-    }
-
-    setTimeout(() => {
-      this._backdrop.classList.add("show");
-    }, 0);
-
-    // Disable scrolling
-    document.body.classList.add("no-scroll");
-    document.body.style.paddingRight = this._getScrollbarWidth() + "px";
-
-    // Attach event listeners
-
-    if (this._allowBackdropClose) {
-      this._backdrop.onclick = (e) => {
-        if (e.target === this._backdrop) {
-          this.close();
-        }
-      };
-    }
-    if (this._allowEscapeClose) {
-      document.addEventListener("keydown", this._handleEscapeKey);
-    }
-
-    this._onTransitionEnd(onOpen);
-
-    return this._backdrop;
-  };
-
-  this._handleEscapeKey = (e) => {
-    const lastModal = Modal.elements[Modal.elements.length - 1];
-    if (e.key === "Escape" && this === lastModal) {
-      this.close();
-    }
-  };
-
-  this._onTransitionEnd = (callback) => {
-    this._backdrop.ontransitionend = (e) => {
-      if (e.propertyName !== "transform") return;
-      if (typeof callback === "function") callback();
-    };
-  };
-
-  this.close = (destroy = destroyOnClose) => {
-    Modal.elements.pop();
-
-    this._backdrop.classList.remove("show");
-
-    if (this._allowEscapeClose) {
-      document.removeEventListener("keydown", this._handleEscapeKey);
-    }
-
-    this._onTransitionEnd(() => {
-      if (this._backdrop && destroy) {
-        this._backdrop.remove();
-        this._backdrop = null;
-        this._modalFooter = null;
-      }
-      // Enable scrolling
-
-      if (!Modal.elements.length) {
-        document.body.classList.remove("no-scroll");
-        document.body.style.paddingRight = "";
-      }
-
-      if (typeof onClose === "function") onClose();
-    });
-  };
-  this.destroy = () => {
-    this.close(true);
-  };
+  this._handleEscapeKey = this._handleEscapeKey.bind(this);
 }
 
-const modal1 = new Modal({
+Modix.prototype._build = function () {
+  const content = this.template.content.cloneNode(true);
+
+  // Create modal elements
+  this._backdrop = document.createElement("div");
+  this._backdrop.className = "modix__backdrop";
+
+  const container = document.createElement("div");
+  container.className = "modix__container";
+
+  this.opt.cssClass.forEach((className) => {
+    if (typeof className === "string") {
+      container.classList.add(className);
+    }
+  });
+
+  if (this._allowButtonClose) {
+    const closeBtn = this._createButton("&times;", "modix__close", () =>
+      this.close(),
+    );
+    container.append(closeBtn);
+  }
+
+  const modalContent = document.createElement("div");
+  modalContent.className = "modix__content";
+
+  // Append content and elements
+  modalContent.append(content);
+  container.append(modalContent);
+
+  if (this.opt.footer) {
+    this._modalFooter = document.createElement("div");
+    this._modalFooter.className = "modix__footer";
+
+    this._renderFooterContent();
+    this._renderFooterButtons();
+
+    container.append(this._modalFooter);
+  }
+
+  this._backdrop.append(container);
+  document.body.append(this._backdrop);
+};
+
+Modix.prototype.setFooterContent = function (html) {
+  this._footerContent = html;
+  this._renderFooterContent();
+};
+
+Modix.prototype.addFooterButton = function (title, cssClass, callback) {
+  const button = this._createButton(title, cssClass, callback);
+  this._footerButtons.push(button);
+  this._renderFooterButtons();
+};
+
+Modix.prototype._renderFooterContent = function () {
+  if (this._modalFooter && this._footerContent) {
+    this._modalFooter.innerHTML = this._footerContent;
+  }
+};
+
+Modix.prototype._renderFooterButtons = function () {
+  if (this._modalFooter) {
+    this._footerButtons.forEach((button) => {
+      this._modalFooter.append(button);
+    });
+  }
+};
+
+Modix.prototype._createButton = function (title, cssClass, callback) {
+  const button = document.createElement("button");
+  button.className = cssClass;
+  button.innerHTML = title;
+  button.onclick = callback;
+
+  return button;
+};
+
+Modix.prototype.open = function () {
+  Modix.elements.push(this);
+
+  if (!this._backdrop) {
+    this._build();
+  }
+
+  setTimeout(() => {
+    this._backdrop.classList.add("modix--show");
+  }, 0);
+
+  // Disable scrolling
+  document.body.classList.add("modix--no-scroll");
+  document.body.style.paddingRight = this._getScrollbarWidth() + "px";
+
+  // Attach event listeners
+  if (this._allowBackdropClose) {
+    this._backdrop.onclick = (e) => {
+      if (e.target === this._backdrop) {
+        this.close();
+      }
+    };
+  }
+
+  if (this._allowEscapeClose) {
+    document.addEventListener("keydown", this._handleEscapeKey);
+  }
+
+  this._onTransitionEnd(this.opt.onOpen);
+
+  return this._backdrop;
+};
+
+Modix.prototype._handleEscapeKey = function (e) {
+  const lastModal = Modix.elements[Modix.elements.length - 1];
+  if (e.key === "Escape" && this === lastModal) {
+    this.close();
+  }
+};
+
+Modix.prototype._onTransitionEnd = function (callback) {
+  this._backdrop.ontransitionend = (e) => {
+    if (e.propertyName !== "transform") return;
+    if (typeof callback === "function") callback();
+  };
+};
+
+Modix.prototype.close = function (destroy = this.opt.destroyOnClose) {
+  Modix.elements.pop();
+
+  this._backdrop.classList.remove("modix--show");
+
+  if (this._allowEscapeClose) {
+    document.removeEventListener("keydown", this._handleEscapeKey);
+  }
+
+  this._onTransitionEnd(() => {
+    if (this._backdrop && destroy) {
+      this._backdrop.remove();
+      this._backdrop = null;
+      this._modalFooter = null;
+    }
+
+    // Enable scrolling
+    if (!Modix.elements.length) {
+      document.body.classList.remove("modix--no-scroll");
+      document.body.style.paddingRight = "";
+    }
+
+    if (typeof this.opt.onClose === "function") this.opt.onClose();
+  });
+};
+
+Modix.prototype.destroy = function () {
+  this.close(true);
+};
+
+Modix.prototype._getScrollbarWidth = function () {
+  if (this._scrollbarWidth) return this._scrollbarWidth;
+
+  const div = document.createElement("div");
+  Object.assign(div.style, {
+    overflow: "scroll",
+    position: "absolute",
+    top: "-9999px",
+  });
+
+  document.body.appendChild(div);
+  this._scrollbarWidth = div.offsetWidth - div.clientWidth;
+  document.body.removeChild(div);
+
+  return this._scrollbarWidth;
+};
+
+const modal1 = new Modix({
   templateId: "modal-1",
   destroyOnClose: false,
   onOpen: () => {
-    console.log("Modal Opned");
+    console.log("Modal 1 opened");
   },
   onClose: () => {
-    console.log("Modal Closed");
+    console.log("Modal 1 closed");
   },
 });
 
 $("#open-modal-1").onclick = () => {
-  const modalElement = modal1.open();
-
-  // const img = modalElement.querySelector("img");
-  // console.log(img);
+  modal1.open();
 };
 
-const modal2 = new Modal({
+const modal2 = new Modix({
   templateId: "modal-2",
-  cssClass: ["class1"],
+  // closeMethods: ['button', 'overlay', 'escape'],
+  cssClass: ["class1", "class2", "classN"],
   onOpen: () => {
-    console.log("Modal Opned");
+    console.log("Modal 2 opened");
   },
   onClose: () => {
-    console.log("Modal Closed");
+    console.log("Modal 2 closed");
   },
 });
 
 $("#open-modal-2").onclick = () => {
   const modalElement = modal2.open();
+
   const form = modalElement.querySelector("#login-form");
   if (form) {
     form.onsubmit = (e) => {
@@ -236,35 +240,49 @@ $("#open-modal-2").onclick = () => {
         email: $("#email").value.trim(),
         password: $("#password").value.trim(),
       };
+
       console.log(formData);
     };
   }
 };
 
-const modal3 = new Modal({
+const modal3 = new Modix({
   templateId: "modal-3",
-  closeMethods: [],
+  closeMethods: ["escape"],
   footer: true,
   onOpen: () => {
-    console.log("Modal Opned");
+    console.log("Modal 3 opened");
   },
   onClose: () => {
-    console.log("Modal Closed");
+    console.log("Modal 3 closed");
   },
 });
 
-// modal3.setFooterContent("<h2>Footer Content</h2>");
+// modal3.setFooterContent("<h2>Footer content</h2>");
 
-modal3.addFooterButton("Cancel", "modal-btn", (e) => {
+modal3.addFooterButton(
+  "Danger",
+  "modix__btn modix__btn--danger modix__btn--pull-left",
+  (e) => {
+    alert("Danger clicked!");
+  },
+);
+
+modal3.addFooterButton("Cancel", "modix__btn", (e) => {
   modal3.close();
 });
 
-modal3.addFooterButton("<span>Agree</span>", "modal-btn primary", (e) => {
-  modal3.close();
-});
-
-modal3.open();
+modal3.addFooterButton(
+  "<span>Agree</span>",
+  "modix__btn modix__btn--primary",
+  (e) => {
+    // Something...
+    modal3.close();
+  },
+);
 
 $("#open-modal-3").onclick = () => {
   modal3.open();
 };
+
+window.Modix = Modix;
